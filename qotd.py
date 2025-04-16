5c55c1886e8910be6d96dccfe6c68d373f9f9c3e
ScarletStar <stievu@gmail.com>
2025-04-16 23:39:34 +0200

    fix prints

--- qotd.py
+++ qotd.py
@@ -3,7 +3,7 @@ import random
 
 quotes = "./quotes.txt"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("0.0.0.0", 17)) # Bind to port 17
+server.bind(("0.0.0.0", 1776)) # Bind to port 17
 server.listen(5)
 
 
@@ -11,15 +11,15 @@ def read_random_line(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
         random_index = random.randint(0, len(lines) - 1)
-        print("getting line" + random_index + "from file")
+        print("getting line" + str(random_index) + "from file")
         return lines[random_index].strip()
 
 
 
 while True:
-    print("server up")
+    print("listening")
     sock, addr = server.accept()
-    print("connecting to socket".join(addr))
+    print("".join(str(addr)))
     quote = read_random_line(quotes)    
     sock.send(f"{quote}\n".encode("utf-8"))
     sock.close()
