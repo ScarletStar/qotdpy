f3fbd5f3c9871fff1e6957e6e337be1b7150c7df
ScarletStar <stievu@gmail.com>
2025-04-16 20:12:50 +0200

    added prints for debugging

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,9 @@ from python:3.13-slim
 # Set the working directory in the container
 WORKDIR /usr/src/app
 
-# Copy the requirements file into the container
-#COPY requirements.txt ./
-
-# Install the dependencies
-#RUN pip install --no-cache-dir -r requirements.txt
-
 # Copy the rest of the application code into the container
 COPY . /usr/src/app
 
 # Specify the command to run the application
-CMD ["python", "./qotd.py"]
+CMD ["python3", "./qotd.py"]
 
--- qotd.py
+++ qotd.py
@@ -11,14 +11,18 @@ def read_random_line(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
         random_index = random.randint(0, len(lines) - 1)
+        print("getting line" + random_index + "from file")
         return lines[random_index].strip()
 
 
 
 while True:
+    print("server up")
     sock, addr = server.accept()
+    print("connecting to socket" + sock + ":" + addr)
     quote = read_random_line(quotes)    
     sock.send(f"{quote}\n".encode("utf-8"))
     sock.close()
+    print("close connection")
 
 
